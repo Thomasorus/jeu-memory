3f537c33f1663c890f219b16d8ab357e085abac9
Thomasorus <contact@thomasorus.com>
2021-02-10 23:29:45 +0100

    *

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# jeu-memory
-Un exercice
+# Jeu memory
+
+Un simple jeu de memory utilisant HTML/CSS/JavaScript pour le front et Express/Mysql pour le back.
+
+## Choix techniques
+
+Le projet est principalement construit en JavaScript natif avec un fichier de template en HTML et un ensemble de fichiers de style. Il utilise express comme serveur et mysql comme base de données. 
+
+En tenant compte que le jeu pourrait servir comme support de cours et que ceux qui y seraient confrontés seraient des développeurs débutants ou junior, j'ai choisi de faire un maximum de logique en JavaScript afin de faire découvrir des fonctionnalités basiques et utiles. Parmi celles-ci:
+
+- `fetch` qui est un moyen simple de faire des requêtes, avec notamment les requêtes asynchrones et `await`. 
+- Les méthodes `querySelector`, `createElement` et `setAttribute` pour la manipulation d'éléments du DOM, au lieu de passer par jquery ou de faire des templates dans des strings.
+- `setInterval` pour la gestion du temps, même si c'est ici très particulier comme usage.
+
+Pour le backend mon choix s'est porté sur express et mysql pour ces raisons :
+
+- Express est le serveur le plus utilisé dans l'industrie dès qu'on touche au backend JS
+- J'ai un temps considéré mongoDB pour stocker les scores/temps des joueurs (pas besoin de BDD relationelle) mais me suis dis que Mysql était plus répandu et surtout que les commandes SQL étaient plus claires et souvent un bon exercice de logique.
+
+J'ai envisagé pendant un temps de faire une version où toute la logique du jeu serait gérée côté backend. Cela a de multiples avantages comme éviter la triche, mais j'ai changé d'avis car cela créait un projet trop complexe pour des élèves débutants. Si le but de l'exercice est de créer un simili support de cours, alors il valait mieux, selon moi, en profiter pour exploiter des 
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port = 8000
 const path = require('path')
 const mysql = require('mysql')
 
+// Afin que Express puisse servir nos fichiers statiques, il convient d'indiquer leurs dossiers.
 app.use(express.static(path.join(__dirname, 'static')))
 app.use(express.static(path.join(__dirname, 'images')))
 
+// Nos identifiants de connexion MYSQL
 const connexion = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -14,31 +16,49 @@ const connexion = mysql.createConnection({
   database: 'memory'
 })
 
+// Une fois l'objet contenant les informations de connexion créé, on peut se connecter.
 connexion.connect(function (err) {
   if (err) throw err
-  console.log('Connected to DB...')
+  console.log('Connecté à la base de données')
 })
 
+
+// Les app.get interceptent les adresses
+// Ainsi un app.get sur / signifie que la racine du site a été atteinte par un utilisateur
+// On peut l'intercepter et décider quoi faire
+
+// Si un utilisateur arrive sur la racine du site, on lui renvoie index.html, notre template de base
 app.get('/', (req, res) => {
   res.sendFile('index.html', {
     root: path.join(__dirname, './')
   })
 })
 
+// L'adresse /getScores ne retourne pas un template mais des données !
+// Elle est donc utilisée pour uniquement récupérer des données, l'affichage
+// du contenu sera géré côté navigateur
 app.get('/getScores', (req, res) => {
+  // On utilise l'object connexion et une requête SQL pour récupérer tous nos scores
   connexion.query('SELECT * FROM scores', function (error, results, fields) {
     if (error) throw error
     return res.send(results)
   })
 })
 
+// Encore une adresse qui ne retourne pas de template HTML
+// Notons l'adresse suivie d'un :temps. Dans ce cas, :temps récupère les données se trouvant
+// après /enregistrerScore
 app.get('/enregistrerScore/:temps', (req, res) => {
+  // On récupère le score via l'onjet req
   const score = req.params.temps
+  // On insère le score dans la base de données via une requête SQL
   connexion.query(`INSERT INTO scores (temps) VALUES (${score})`, function (error, results, fields) {
     if (error) throw error
   })
 })
 
+// Express a besoin de cette fonction pour 'écouter' ce qui se passe, notamment les adresses
+// que nous utilisons
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
+  console.log(`Écoute de l'adresse http://localhost:${port}`)
 })
