11a2358b2e0528a22bdc4a5fe7e39bdb2ea8c6d5
Thomasorus <contact@thomasorus.com>
2021-02-11 19:52:53 +0100

    Added comments

--- static/main.js
+++ static/main.js
@@ -1,6 +1,19 @@
-// LES VARIABLES D'ÉTAT
-// Ces variables sont partagées par l'ensemble du jeu et seront modifiées en fonction des actions du joueur
+//=============================================================
 
+// JEU DE MEMORY
+
+//=============================================================
+
+// La majorités des actions du jeu sont effectuées dans ce fichier via des fonctions.
+
+
+
+
+//=============================================================
+// LES VARIABLES D'ÉTAT
+//=============================================================
+
+// Ces variables sont partagées par l'ensemble du jeu et seront modifiées en fonction des actions du joueur
 // Premièrement, nous stockons quelles cartes le joueur a retourné
 let premiereCarte = null
 let secondeCarte = null
@@ -12,11 +25,20 @@ let bloquerGrille = false
 let cartesTrouvees = 18
 // Le temps alloué à la partie
 let tempsDeJeu = 300
+// Est-ce que la musique est en train d'être jouée ?
+let musiqueEnCours = false
+
+
+
+//=============================================================
+// LES FONCTIONS PRINCIPALES CÔTÉ FRONTEND
+//=============================================================
+
 
 // Cette fonction met en place une partie de memory !
 // Comme notre HTML ne contient aucune carte, il va falloir les créer et les afficher.
 // On lui donne en argument le nombre de cartes que l'on veut créer (ici 18 car nous avons 18 fruits)
-function creationJeu (nombreDeCartes) {
+function creationJeu(nombreDeCartes) {
   // On crée un tableau pour stocker l'ensemble de nos cartes.
   const toutesLesCartes = []
 
@@ -32,7 +54,7 @@ function creationJeu (nombreDeCartes) {
   }
 
   // Problème : nos cartes sont dans l'ordre ! On doit les mélanger !
-  melangerCartes(toutesLesCartes)
+  // melangerCartes(toutesLesCartes)
 
   // On va chercher l'endroit dans la page où insérer les cartes
   const tableauDeJeu = document.querySelector('#tableau')
@@ -47,7 +69,7 @@ function creationJeu (nombreDeCartes) {
   document.querySelector('article').setAttribute('data-visible', true)
 
   // On cache le bouton qui permet de lancer le jeu
-  document.querySelector('#lancerJeu').setAttribute('data-visible', false)
+  document.querySelector('#accueil').setAttribute('data-visible', false)
 
   // On lance le temps du jeu
   lancerTimer()
@@ -56,7 +78,7 @@ function creationJeu (nombreDeCartes) {
 // Cette fonction sert à créer nos cartes HTML via du JavaScript!
 // Pour ce faire on va utiliser les méthodes de manipulations d'éléments HTML
 // On lui donne en argument la carte actuelle
-function creationCarte (numeroCarte) {
+function creationCarte(numeroCarte) {
   // On veut que chaque carte soit dans un conteneur
   // On utilise donc la méthode createElement pour créer une div.
   const conteneurCarte = document.createElement('div')
@@ -79,7 +101,7 @@ function creationCarte (numeroCarte) {
 
   // Enfin évidemment, on créer l'attribut src qui permet de renseigner
   // l'url de l'image
-  carte.setAttribute('src', `${numeroCarte}.png`)
+  carte.setAttribute('src', `${numeroCarte}.jpg`)
 
   // Il reste à mettre notre image dans notre conteneur
   // On peut le faire avec la fonction append
@@ -89,37 +111,9 @@ function creationCarte (numeroCarte) {
   return conteneurCarte
 }
 
-// Cette fonction permet de changer l'ordre des éléments d'un tableau
-// Dans notre cas, on l'utilise pour mélanger les cartes
-function melangerCartes (tableauDeCartes) {
-
-  // On parcourt le tableau avec une boucle for
-  for (let i = tableauDeCartes.length - 1; i > 0; i--) {
-
-    // Nous n'allons pas créer un nouveau tableau mais bouger l'ordre de ses élements.
-    // On crée un nombre au hasard entre 0 et 1 via la méthode Math.random().
-    // On le multiplie par le numéro de notre boucle + 1 (afin d'éviter de multiplier par 0)
-    const nombreInconnu = Math.random() * (i + 1)
-
-    // On arrondit au plus petit avec la méthode Math.floor
-    const nombreInconnuEntier = Math.floor(nombreInconnu)
-
-    // Afin de la réutiliser plus tard, on stocke notre carte actuelle dans une variable
-    const carteActuelle = tableauDeCartes[i]
-    const carteAuHasard = tableauDeCartes[nombreInconnuEntier]
-
-    // On remplace la carte de notre boucle par la carte prise au hasard
-    tableauDeCartes[i] = carteAuHasard
-    // On remplace la carte prise au hasard par notre carte actuelle
-    tableauDeCartes[nombreInconnuEntier] = carteActuelle
-  }
-
-  // Les éléments du tableau ont été mélangés, on le renvoie !
-  return tableauDeCartes
-}
 
 // Cette fonction permet de lancer et afficher le temps de jeu
-function lancerTimer () {
+function lancerTimer() {
   // On définit d'abord le temps alloué au joueur
 
   // On sélectionne où afficher le temps, ici via la balise HTML <progress>
@@ -130,17 +124,17 @@ function lancerTimer () {
 
   // On stocke notre compteur dans une variable et on lance le setInterval
   const compteur = setInterval(function () {
-    // En premier, on vérifie si le temps est à zéro ou si les cartes ont été trouvées
+    // En premier, on vérifie si le temps est à zéro ou si toutes les cartes ont été trouvées
     if (tempsDeJeu <= 0 || cartesTrouvees <= 0) {
       // Si c'est le cas, on arrête le setInterval avec la méthode clearInterval
       clearInterval(compteur)
       // Et on bloque la grille du jeu, afin que le joueur ne puisse pas continuer
       bloquerGrille = true
 
-      // Si toutes les cartes ont été trouvées, on enregistre le score
-      if (cartesTrouvees === 0) {
-        const temps = 300 - tempsDeJeu
-        enregistrerScores(temps)
+      // Si toutes les cartes n'ont pas été trouvées après le temps imparti, on joue un son
+      if (cartesTrouvees > 0) {
+        jouerSon('defaite', 1)
+        montrerDefaite()
       }
     }
     // A chaque intervale d'une seconde, on diminue de 1 seconde le temps restant
@@ -155,7 +149,7 @@ function lancerTimer () {
 // Chaque fois que l'utilisateur va regarder une carte, il va falloir mettre à jour
 // l'état du jeu et déclencher des actions en fonction de celui-ci.
 // En premier lieu, on donne en argument la carte que le joueur vient de cliquer.
-function voirCarte (carte) {
+function voirCarte(carte) {
   // Mais est-ce que le joueur a le droit de retourner cette carte ?
   // Pour le savoir, on vérifie la variable bloquerGrille
   // Si elle est sur True, alors le joueur n'a pas le droit et on arrête tout.
@@ -163,8 +157,14 @@ function voirCarte (carte) {
     return
   }
 
+  // Pour empêcher le joueur de cliquer deux fois sur la même carte, on vérifie que
+  // celle qu'il a cliqué n'est pas la même que la précédente et si oui, on ne fait rien.
+  if (carte === premiereCarte) {
+    return
+  }
+
   // Mais s'il a le droit... C'est là qu'on vérifie où il en est dans la partie.
-  
+
   // Première interrogation : est-ce la première ou la seconde carte qu'il retourne ?
   // Pour le savoir, on vérifie la variable uneCarteEstRetournee.
   // Si elle est fausse, cela veut dire que c'est la première fois qu'il tourne une carte.
@@ -177,7 +177,6 @@ function voirCarte (carte) {
     premiereCarte = carte
 
     // Et puis c'est tout ! Il n'y a rien de plus à faire quand c'est la première carte.
-
   } else {
     // Et dans le cas où le joueur avait déjà retourné une carte ?
     // On lui montre l'image de la carte évidemment
@@ -210,9 +209,18 @@ function voirCarte (carte) {
       // On retire une parie de cartes de notre total
       cartesTrouvees -= 1
 
+      // On joue un petit son pour fêter la réussite !
+      document.querySelector('#woof').play()
+
       // On vérifie si le joueur a trouvé toutes les paires
       if (cartesTrouvees === 0) {
-        console.log('fin du game')
+        enregistrerScores(tempsDeJeu)
+        if (musiqueEnCours) {
+          jouerSon('musique', null)
+          musiqueEnCours = false
+        }
+        jouerSon('victoire', 1)
+        montrerVictoire()
       }
     } else {
       // Mais si les deux cartes retournées ne correspondent pas, que faire ?
@@ -238,9 +246,13 @@ function voirCarte (carte) {
   }
 }
 
+//=============================================================
+// LES FONCTIONS QUI INTERROGENT LA BASE DE DONNÉES
+//=============================================================
+
 // La fonction RecupererScores interroge le serveur pour avoir les scores
 // Puis elle les affiche dans la page
-async function RecupererScores () {
+async function RecupererScores() {
   // On fait un appel au serveur pour récupérer les cores en utilisant la méthode FETCH
   const requeteDesScores = await fetch('getScores').then(function (response) {
     // On récupère le résultat de la requête et on le transforme en format JSON
@@ -273,9 +285,89 @@ async function RecupererScores () {
 }
 
 // Cette fonction envoie au serveur le temps réalisé par le joueur
-function enregistrerScores (temps) {
+function enregistrerScores(temps) {
   const requeteDesScores = fetch(`enregistrerScore/${temps}`).then(function (response) {
     // On récupère le résultat de la requête et on le transforme en format JSON
     return response.json()
   })
+}
+
+
+//=============================================================
+// LES FONCTIONS SECONDAIRES ET UTILITAIRES
+//=============================================================
+
+// Cette fonction permet de changer l'ordre des éléments d'un tableau
+// Dans notre cas, on l'utilise pour mélanger les cartes
+function melangerCartes(tableauDeCartes) {
+  // On parcourt le tableau avec une boucle for
+  for (let i = tableauDeCartes.length - 1; i > 0; i--) {
+    // Nous n'allons pas créer un nouveau tableau mais bouger l'ordre de ses élements.
+    // On crée un nombre au hasard entre 0 et 1 via la méthode Math.random().
+    // On le multiplie par le numéro de notre boucle + 1 (afin d'éviter de multiplier par 0)
+    const nombreInconnu = Math.random() * (i + 1)
+
+    // On arrondit au plus petit avec la méthode Math.floor
+    const nombreInconnuEntier = Math.floor(nombreInconnu)
+
+    // Afin de la réutiliser plus tard, on stocke notre carte actuelle dans une variable
+    const carteActuelle = tableauDeCartes[i]
+    const carteAuHasard = tableauDeCartes[nombreInconnuEntier]
+
+    // On remplace la carte de notre boucle par la carte prise au hasard
+    tableauDeCartes[i] = carteAuHasard
+    // On remplace la carte prise au hasard par notre carte actuelle
+    tableauDeCartes[nombreInconnuEntier] = carteActuelle
+  }
+  // Les éléments du tableau ont été mélangés, on le renvoie !
+  return tableauDeCartes
+}
+
+// De nombreux sons sont joués pendant une partie.
+// Afin de rendre leur utilisation plus facile, on crée cette fonction jouerSon qui va s'occuper
+// de détecter s'il faut mettre en lecture ou en pause
+// Elle prend un fichier et le volume en argument
+function jouerSon(fichier, volume) {
+  const son = document.querySelector(`#${fichier}`)
+  if (volume) {
+    son.volume = volume
+  }
+  if (!son.paused) {
+    son.pause()
+    if (fichier === 'musique') {
+      musiqueEnCours = false
+    }
+  } else {
+    son.play()
+    if (fichier === 'musique') {
+      musiqueEnCours = true
+    }
+  }
+}
+
+
+// L'écran de victoire du jeu !
+// C'est une simple fonction insérant et manipulant des éléments HTML
+function montrerVictoire() {
+  const gif = document.createElement('img')
+  gif.setAttribute('src', 'snoop-dog.gif')
+  gif.setAttribute('class', 'snoop-dog')
+  document.querySelector('#tableau').append(gif)
+
+  // querySelectorAll permet de récupérer tous les éléments de la page
+  // Il renvoie un tableau sur lequel il ne reste plus qu'à itérer
+  const toutesLesCartes = document.querySelectorAll('.conteneur-carte')
+  toutesLesCartes.forEach(carte => {
+    carte.setAttribute('class', 'conteneur-carte rotation')
+  })
+}
+
+
+// L'écran de victoire du jeu !
+// C'est une simple fonction insérant et manipulant des éléments HTML
+function montrerDefaite() {
+  const image = document.createElement('img')
+  image.setAttribute('src', 'jail.jpg')
+  image.setAttribute('class', 'snoop-dog')
+  document.querySelector('#tableau').append(image)
 }
