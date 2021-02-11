216bac1ab5d5680aea80f6793fd2975caf5f3183
Thomasorus <contact@thomasorus.com>
2021-02-11 21:48:11 +0100

    updated index

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.static(path.join(__dirname, 'images')))
 // Nos identifiants de connexion MYSQL
 const connexion = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'rootroot',
+  user: '',
+  password: '',
   database: 'memory'
 })
 
